f6caa4358b0c3e2b83aaa795b31e64871844b749
dwafotapa <dominique.wafotapa@gmail.com>
2018-01-19 17:05:02 +0100

    Add styling to mash page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,34 @@
 <template>
   <main>
-    <div class="cat-wrapper">
-      <img :src="randomCatUrl" alt=""/>
+    <div class="left-half">
+      <img v-if="cats.length > 0" :src="cats[leftCatIndex].url" :alt="cats[leftCatIndex].url"/>
     </div>
-    <div class="cat-wrapper">
-      <img :src="randomCatUrl" alt=""/>
+    <div class="right-half">
+      <img v-if="cats.length > 0" :src="cats[rightCatIndex].url" :alt="cats[rightCatIndex].url"/>
     </div>
-    <div class="topcats-wrapper">
+    <div class="topcats">
       <a href="/topcats">Top Cats</a>
     </div>
   </main>
 </template>
 
 <script>
+import { getRandomInt } from '@/utils/random'
+
 export default {
   computed: {
-    randomCatUrl () {
-      return this.$store.state.cats.all[0]
+    cats () {
+      return this.$store.state.cats.all
+    },
+    leftCatIndex () {
+      return getRandomInt(this.cats.length)
+    },
+    rightCatIndex () {
+      let index = getRandomInt(this.cats.length)
+      while (index === this.leftCatIndex) {
+        index = getRandomInt(this.cats.length)
+      }
+      return index
     }
   },
   created () {
@@ -28,13 +40,27 @@ export default {
 <style scoped>
 main {
   display: flex;
+  height: 100vh;
 }
 
-.cat-wrapper {
-  flex: 0 1 100%;
+.left-half, .right-half {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  justify-content: center;
+  width: 50%;
 }
 
-.topcats-wrapper {
+.left-half img, .right-half img {
+  width: 40%;
+}
+
+.left-half {
+  background-color: #ededed;
+}
+
+.topcats {
   bottom: 10px;
   left: 0;
   right: 0;
@@ -42,13 +68,13 @@ main {
   position: absolute;
 }
 
-.topcats-wrapper a {
+.topcats a {
   color: inherit;
   font-weight: bold;
   text-decoration: none;
 }
 
-.topcats-wrapper a:hover {
+.topcats a:hover {
   text-decoration: underline;
 }
 </style>
