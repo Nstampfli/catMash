67140d41e2686d6cde8b6a722d678131608b0a44
dwafotapa <dominique.wafotapa@gmail.com>
2018-01-25 20:55:21 +0100

    Publish to docs folder

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/catmash/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/catmash/',
 
     /**
      * Source Maps
